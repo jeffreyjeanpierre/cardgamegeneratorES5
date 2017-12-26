b69ae3f9cda888a2a4e326fad38a12cbd6028272
jeffreyjeanpierre <jitbag@gmail.com>
2017-12-25 22:14:14 -0500

    Completed Card Class
    Implementation of card is done but usage needs tweaking to follow the suit/value formal parameters instead of passing a number. Also need to add type validation to Card and Hand instances.

--- cardGameModuleES5.js
+++ cardGameModuleES5.js
@@ -103,11 +103,23 @@ function Card(configuration) {
     this.getSuit = getSuit;
     this.getValue = getValue;
     this.getSuitAsString = getSuitAsString;
+    this.getValueAsString = getValueAsString;
+    this.toString = toString;
     setupCards(configuration);
 
     // helper functions
     function getSuit(){ return suit; }
     function getValue(){ return value; }
+    function toString() { 
+        if (suit == JOKER) {
+            if (value == 1) {
+                return "Joker";
+            }
+            else { 
+                return getValueAsString() + " of " + getSuitAsString();
+            }
+        }
+    }
     function getSuitAsString(){ 
         switch(suit){
             case SPADES: return "Spades";
@@ -116,7 +128,29 @@ function Card(configuration) {
             case CLUBS: return "Clubs";
             default: return "Joker";
         }
-     }
+    }
+    function getValueAsString() {
+        if (suit == JOKER){
+            return "" + value; // necessary?
+        }
+        else {
+            switch(value) {
+                case 1: return "Ace";
+                case 2: return "2";
+                case 3: return "3";
+                case 4: return "4";
+                case 5: return "5";
+                case 6: return "6";
+                case 7: return "7";
+                case 8: return "8";
+                case 9: return "9";
+                case 10: return "10";
+                case 11: return "11";
+                case 12: return "12";
+                default: return "King";
+            }
+        }
+    }
     function setupCards(config) {
         if (config) {
             if (config.suit != SPADES && config.suit != HEARTS && config.suit != DIAMONDS && 
