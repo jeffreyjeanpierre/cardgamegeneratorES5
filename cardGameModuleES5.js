459bf350fc1b8f5db127546499b85609d3390f29
jeffreyjeanpierre <jitbag@gmail.com>
2017-12-25 22:02:10 -0500

    Add files via upload

--- cardGameModuleES5.js
+++ cardGameModuleES5.js
@@ -76,7 +76,64 @@ function Deck (includeJokers) {
 }
 
 /* Card Class */
-function Card(amount) { return new Array(amount); }
+function Card(configuration) {
+    // face cards and suits
+    const KING = 13;
+    const QUEEN = 12;
+    const JACK = 11;
+    const ACE = 1;
+    const JOKER = 4;
+    const CLUBS = 3;
+    const DIAMONDS = 2;
+    const HEARTS = 1;
+    const SPADES = 0;
+
+    const suit;
+    const value;
+
+    this.KING = KING;
+    this.QUEEN = QUEEN;
+    this.JACK = JACK;
+    this.ACE = ACE;
+    this.JOKER = JOKER;
+    this.CLUBS = CLUBS;
+    this.DIAMONDS = DIAMONDS;
+    this.HEART = HEART;
+    this.SPADES = SPADES;
+    this.getSuit = getSuit;
+    this.getValue = getValue;
+    this.getSuitAsString = getSuitAsString;
+    setupCards(configuration);
+
+    // helper functions
+    function getSuit(){ return suit; }
+    function getValue(){ return value; }
+    function getSuitAsString(){ 
+        switch(suit){
+            case SPADES: return "Spades";
+            case DIAMONDS: return "Diamonds";
+            case HEARTS: return "Hearts";
+            case CLUBS: return "Clubs";
+            default: return "Joker";
+        }
+     }
+    function setupCards(config) {
+        if (config) {
+            if (config.suit != SPADES && config.suit != HEARTS && config.suit != DIAMONDS && 
+                config.suit != CLUBS && config.suit != JOKER)
+                console.log("Illegal playing card suit");
+            if (config.suit != JOKER && (config.suit < 1 || config.suit > 13))
+                console.log("Illegal playing card value");
+            value = config.value;
+            suit = config.suit;
+        }
+        else {
+            suit = JOKER;
+            value = 1;
+        }
+    }
+
+}
 
 /* Hand Class */
 function Hand() {
@@ -87,11 +144,100 @@ function Hand() {
     this.getCard = getCard;
     this.sortBySuit = sortBySuit;
     this.sortByValue = sortByValue;
+    setupHand();
 
-    function clear(){}
-    function removeCard(card, handPosition){}
-    function addCard(card, handPosition){}
-    function getCard(handPosition){}
-    function sortBySuit(){}
-    function sortByValue(){}
+    // Private attributes
+    var hand;
+    var maxHandLength;
+
+
+    // Private functionality
+    function clear(){ hand = []; }
+    function removeCardByPosition(handPosition){
+        var position = card.position;
+        if(!position) { console.log('can not remove null card to a hand'); return; }
+        hand.splice(position, 1);
+    }
+    function removeCardByCard(card){
+        var position = card.position;
+        if(!position || position < 0 || position > hand.length) { console.log('can not remove an invalid card from a hand'); return; }
+        hand.splice(position, 1);
+    }
+    function addCardByPosition(handPosition){
+        var newCard = {
+            position: handPosition
+            // add other keys for new card object here
+        };
+        
+        if (!position || position < 0 || position > hand.length) { console.log('can not add null card to a hand'); return; }
+         // create fn to get new card which reads length of hand
+        // if (maxHandLength && (hand.length + 1) <= maxHandLength){
+
+        // }
+        hand[hand.length] = newCard;
+    }
+    function addCardByCard(card){ // TODO: finish addCard implementation
+        var newCard = {
+            position: card.position
+            // add other keys for new card object here
+        };
+        
+        if (!position || position < 0 || position > hand.length) { console.log('can not add null card to a hand'); return; }
+         // create fn to get new card which reads length of hand
+        // if (maxHandLength && (hand.length + 1) <= maxHandLength){
+
+        // }
+        hand[hand.length] = newCard;
+        
+    }
+    function getCardByPosition(handPosition){
+        var position = handPosition;
+        if (!position || position < 0 || position > hand.length) { console.log('can not get invalid card from a hand'); return; }
+        return hand[position];
+    }
+    function getCardByCard(card){
+        var position = card.position;
+        if (!position || position < 0 || position > hand.length) { console.log('can not get invalid card from a hand'); return; }
+        return hand[position];
+    }
+    function sortBySuit(){
+        var newHand = [];
+        while (hand.length > 0) {
+            var pos = 0;  // Position of minimal card.
+            var card = hand[0];  // Minimal card.
+            for (var i = 1; i < hand.length; i++) {
+                var card1 = hand[i];
+                if ( card1.getSuit() < card.getSuit() ||
+                        (card1.getSuit() == card.getSuit() && card1.getValue() < card.getValue()) ) {
+                    pos = i;
+                    card = card1;
+                }
+            }
+            hand.splice(pos, 1);
+            newHand.push(card);
+        }
+        hand = newHand;
+    }
+    function sortByValue(){
+        var newHand = [];
+        while (hand.length > 0) {
+            var pos = 0;  // Position of minimal card.
+            var card = hand[0];  // Minimal card.
+            for (var i = 1; i < hand.length; i++) {
+                var card1 = hand[i];
+                if ( card1.getValue() < card.getValue() ||
+                        (card1.getValue() == card.getValue() && card1.getSuit() < card.getSuit()) ) {
+                    pos = i;
+                    card = card1;
+                }
+            }
+            hand.splice(pos, 1);
+            newHand.push(c);
+        }
+        hand = newHand;
+    }
+    function setupHand(){ hand = []; }
+    function getCardCount() {
+        return hand.length-1;
+    }
 }
